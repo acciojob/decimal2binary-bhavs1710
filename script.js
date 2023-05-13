12411df298b4c0a8ddc42f4f5a80c970c227a450
Bhawna Sharma <119921762+bhavs1710@users.noreply.github.com>
2023-05-13 12:18:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
 function threeSum(arr, target) {
 // write your code here
-	function convertToBinary(x) {
-    let bin = 0;
-    let rem, i = 1, step = 1;
-    while (x != 0) {
-        rem = x % 2;
-        console.log(
-            `Step ${step++}: ${x}/2, Remainder = ${rem}, Quotient = ${parseInt(x/2)}`
-        );
-        x = parseInt(x / 2);
-        bin = bin + rem * i;
-        i = i * 10;
-    }
-    console.log(`Binary: ${bin}`);
+{
+	let arr = new Array(33);
+
+	// counter for binary array
+	let i = 0;
+	while (n > 0) {
+
+		// storing remainder in binary array
+		arr[i] = n % 2;
+		n = Math.floor(n / 2);
+		i++;
+	}
+
+	// printing binary array in reverse order
+	for (let j = i - 1; j >= 0; j--)
+		document.write(arr[j]);
 }
+
+
+	let n = 17;
+	decToBinary(n);
 
 // take input
 let number = prompt('Enter a decimal number: ');
