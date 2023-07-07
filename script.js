806d6a62f3f1b9bb9cda8aa2ba5c5cbcec06ed3c
Umakant Dwivedi <78226651+ukdgit@users.noreply.github.com>
2023-07-07 15:14:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,5 @@
-function threeSum(arr, target) {
-// write your code here
-
-
-	function decimalToBinary(decimal) {
-  // Check if the input is a valid decimal number
-  if (isNaN(decimal) || decimal < 0 || decimal % 1 !== 0) {
-    return "Invalid input. Please enter a non-negative integer.";
-  }
-
-  // Handle the special case of decimal number 0
-  if (decimal === 0) {
-    return "0";
-  }
-
-  let binary = "";
-
-  // Convert decimal to binary
-  while (decimal > 0) {
-    binary = (decimal % 2) + binary;
-    decimal = Math.floor(decimal / 2);
-  }
-
-  return binary;
+function decimalToBinary(num) {
+  // Write your code here
 }
 
-// Example usage
-const decimalNumber = 42;
-const binaryNumber = decimalToBinary(decimalNumber);
-console.log("Binary representation:", binaryNumber);
-
-
-
-
-
-
-
+window.decimalToBinary = decimalToBinary;
